70bfc42c1328f0f89d628d1f66ed2975c641e31d
Sidoryak-Ostap <ostapsidorak@gmail.com>
2026-05-17 17:09:12 +0300

    remove tab

--- src/components/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -24,7 +24,7 @@ const NAV_ITEMS = [
     icon: CircleDollarSign,
     to: ROUTES.dashboard.pricing,
   },
-  { labelKey: 'dashboard.sidebar.nav.profitability', icon: BanknoteArrowUp, to: '' },
+  // { labelKey: 'dashboard.sidebar.nav.profitability', icon: BanknoteArrowUp, to: '' },
 ];
 
 const PREFERENCE_ITEMS = [
